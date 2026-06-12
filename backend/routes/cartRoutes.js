6a633501ed90a93e0cc12812e21998570cef52f0
M.Amir Shabbir <m.amirshabbir99@gmail.com>
2026-06-12 15:50:44 +0500

    .

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router  = express.Router();
 const { getCart, addToCart, updateCartItem, removeFromCart, clearCart } = require('../controllers/cartController');
 const { protect } = require('../middleware/authMiddleware');
-
+//cart routes
 router.get('/',                 protect, getCart);
 router.post('/',                protect, addToCart);
 router.put('/:productId',       protect, updateCartItem);
